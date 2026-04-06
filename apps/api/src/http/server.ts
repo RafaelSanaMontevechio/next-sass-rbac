33792a572d42d81adf9e27bfd678b2e8488821d5
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2026-04-06 13:58:56 -0300

    feat: add fastify app

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,29 @@
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+
+import { createAccount } from './routes/auth/create-account';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: '*',
+});
+
+app.register(createAccount);
+
+app.listen({ port: 3333 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is running!`);
+});
--- apps/api/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { defineAbilitiesFor, projectSchema } from '@sass/auth';
-
-const ability = defineAbilitiesFor({ role: 'ADMIN', id: '' });

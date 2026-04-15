a5cafa2021307db102b8a7c68e695c3c7a684924
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2026-04-15 14:16:53 -0300

    feat: add create user route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -3,6 +3,10 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
 import { z } from 'zod';
 
+import { hash } from 'bcryptjs';
+
+import { prisma } from '@/lib/prisma';
+
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
@@ -15,8 +19,32 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created';
+    async (request, replay) => {
+      const { name, email, password } = request.body;
+
+      const userWithSameEmail = prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (userWithSameEmail) {
+        return replay.status(400).send({
+          message: 'User with the same email already exists.',
+        });
+      }
+
+      const passwordHash = await hash(password, 6);
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      });
+
+      return replay.status(201).send();
     }
   );
 }
